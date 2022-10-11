fe11fe3babbdb61bbdb6522505a06e3e64405089
olhazamlynska <olya.sonechko@gmail.com>
2022-10-11 22:14:42 +0300

    start filles

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 import './css/styles.css';
-
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { fetchCountries } from './js/fetchCountries';
 const DEBOUNCE_DELAY = 300;
+
+// fetchCountries();
---
+++ src/js/fetchCountries.js
@@ -0,0 +1,11 @@
+// export function fetchCountries() {
+//   return fetch(
+//     `https://restcountries.com/v3.1/name/ukraine?fields=name,capital,population,flags,languages`
+//   )
+//     .then(response => {
+//       response.json;
+//     })
+//     .then(data => {
+//       console.log(data);
+//     });
+// }
